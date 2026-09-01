c0c39b54966d63abf88ba50202452f3bea26d365
bitu1212 <bituk8591@gmail.com>
2026-09-01 17:31:29 +0000

    basic test.js

---
+++ .devcontainer/01_Basics/test.js
@@ -0,0 +1,17 @@
+const accountID = 12345
+let emailID = "bantu@12435"
+var accountPassword = "1233"
+Accountcity = "Bihar"
+
+// accountID = 2
+console.log(accountID);
+
+accountEmail = "agb2@";
+accountPassword = "32323";
+accountcity = "Gopalganj";
+
+console.log(accountEmail);
+console.table([accountEmail ,accountPassword ,accountcity]);
+
+
+
