38fcfdd77459c72bee938c8f8a4a30965d8a743e
bitu1212 <bituk8591@gmail.com>
2026-09-01 18:11:44 +0000

    datatypes

---
+++ .devcontainer/01_Basics/01_datatypes.js
@@ -0,0 +1,12 @@
+let userName = "bitu"
+let age = 29
+let city = "Bihar"
+
+// string => ""
+// boolean => true or false
+// null => standalone value
+// symbol => uniqe
+// bigint => 
+// number => 2 to power 53
+
+console.log(typeof city);
