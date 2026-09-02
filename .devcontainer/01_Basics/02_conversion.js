421ac76e409ae9e6b3cc24622004fefc10f0030e
bitu1212 <bituk8591@gmail.com>
2026-09-02 06:35:30 +0000

    conversion

---
+++ .devcontainer/01_Basics/02_conversion.js
@@ -0,0 +1,23 @@
+// let score = "true"
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+// console.log(typeof score);
+// let myName = 0
+
+// let booleanmyName = Boolean(myName);
+// console.log(booleanmyName);
+// 1 >= true
+// 0 >= false
+// string >= true
+
+let someNumber = 43
+
+// let stringNumber = String(someNumber); **  first letter is always capital varriable's value in conversion
+console.log(stringNumber);
+console.log(typeof stringNumber);
