9e4bf9c17aeae8f52fd06e745a278d5cb7e424bf
bitu1212 <bituk8591@gmail.com>
2026-09-02 08:02:06 +0000

    basic2

--- .devcontainer/01_Basics/02_conversion.js
+++ .devcontainer/01_Basics/02_conversion.js
@@ -10,7 +10,7 @@
 // console.log(typeof score);
 // let myName = 0
 
-// let booleanmyName = Boolean(myName);
+// let booleanmyName = Boolean(myName);   Boolean bhi first letter capital hai
 // console.log(booleanmyName);
 // 1 >= true
 // 0 >= false
@@ -19,5 +19,37 @@
 let someNumber = 43
 
 // let stringNumber = String(someNumber); **  first letter is always capital varriable's value in conversion
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************************Operations********************************************
+
+let value = 4
+let negValue = -value;
+// console.log(negValue);
+
+str1 = "Rohit"
+str2 = "Bittu"
+// str3 = str1 + str2 (agar do string ko add karna ho to "" me nhi likhte hai)
+
+// console.log(str3);
+
+// console.log("1" + 2);  ** yaha "1" string hai to aage wala value ko v string mana jayega
+// console.log(2 + "1");
+// console.log("1" +2+2);
+// console.log(1+2+"2"); ** yaha sirf last wala string hai baki sab number hai so 32
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+
+// console.log(gameCounter);
+
+// console.log("2" >1);  yaha js string ko number me change kar diya'
+console.log("2" >=1);
+
+console.log(2 >= "1");
+
+
+
+
