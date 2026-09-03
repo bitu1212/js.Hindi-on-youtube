9769d73e7948fc23f4436842c36383c121059940
bitu1212 <bituk8591@gmail.com>
2026-09-03 12:28:01 +0000

    math and numer

---
+++ .devcontainer/01_Basics/04_number_and_math.js
@@ -0,0 +1,35 @@
+const balance = 500
+// console.log(balance);
+
+const otherNumber = 123.8989
+// console.log(otherNumber.toPrecision(4));
+
+const rupees = 100000
+
+// console.log(rupees.toLocaleString());
+
+// *********************Math***********************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.6));
+// console.log(Math.floor(5.6));
+// console.log(Math.ceil(5.6));
+// console.log(Math.max(1,2,34,4,4));
+// console.log(Math.min(1,2,3,4,5,0));
+
+console.log(Math.random());
+
+console.log((Math.random()*10 )+1);
+console.log(Math.floor(Math.random()*10 )+1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()* (max - min +1)) + min);
+
+
+
+
+
+
+
