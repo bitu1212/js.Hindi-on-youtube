3fc0c9b3eaf47e6ef40680ee7f544aa104bbec86
bitu1212 <bituk8591@gmail.com>
2026-09-07 05:12:08 +0000

    change in 01_Basics

--- .devcontainer/01_Basics/03_string.js
+++ .devcontainer/01_Basics/03_string.js
@@ -3,21 +3,33 @@ const repoCount = 50
 
 // console.log(name + repoCount); **** not good
 
-console.log(`my name is ${name} and my repoCount is ${repoCount}`);
+// console.log(`my name is ${name} and my repoCount is ${repoCount}`);
 
 const gameName = new String('Bituyadav')
 // console.log(gameName[2]);
 // console.log(gameName.__proto__);
 // console.log(gameName.length);
-console.log(gameName.toUpperCase());
+// console.log(gameName.toUpperCase());
 const newString = gameName.substring(0 , 4);
-console.log(newString);
+// console.log(newString);
 
 const anotherString = gameName.slice(0 , 4);
-console.log(anotherString);
+// console.log(anotherString);
 
 const url = "http://bitu.com//%20bittu.com";
 
-console.log(url.replace('%20' , '__'));
+// console.log(url.replace('%20' , '__'));
+
+let Name = "Bittukumar"
+
+// console.log(Name);
+let Name2 = "   Bittukumar "
+console.log(Name2);
+console.log(Name2.trim());
+// console.log(url.includes("bittu")); ******* .include value ko khojta hai
+
+// console.log(gameName.split('_')); ******** .split ka use hota hai space dene ke liy
+
+
 
 
