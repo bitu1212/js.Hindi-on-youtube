aea6cd20ec9c95d93d248b5ad2d42bbd997084e0
bitu1212 <bituk8591@gmail.com>
2026-09-07 12:06:08 +0000

    objects

---
+++ .devcontainer/02_Basics/03_Objects.js
@@ -0,0 +1,34 @@
+//Objects*******
+// objects me jo varriable declear kara jata wo hidden string me hota hai
+
+
+const userName ={
+    name: "Bittu",
+    "full Name": "Bittu yadav",
+    age: 28,
+    Location: "Delhi",
+    Email:    "kumar@123",
+    isLogged:  false,
+    lastoggin: "Monday"
+
+
+}
+
+// console.log(userName.Email);
+// console.log(userName["Email"]);
+// console.log(userName["full Name"]);
+
+
+userName.Email = "yadav@12.com"
+// Object.freeze(userName)
+userName.Email = "kumar@123gmai.com"
+// console.log(userName);
+
+JsUser.greeting =function(){
+    console.log("Hello world");
+    
+}
+
+
+
+console.log(JsUser.greeting());
