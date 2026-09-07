30ad9336ba0538f68e929300d3209180d8511d55
bitu1212 <bituk8591@gmail.com>
2026-09-07 05:14:04 +0000

    01_Basic

---
+++ .devcontainer/01_Basics/06_Datesinjs.js
@@ -0,0 +1,13 @@
+// *********** Dates*************
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+//  console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2026 ,0, 19);
+
+let myCreatedDate = new Date(2026 ,0 ,19 ,7 ,5,30 );
+
+console.log(myCreatedDate.toLocaleString());
