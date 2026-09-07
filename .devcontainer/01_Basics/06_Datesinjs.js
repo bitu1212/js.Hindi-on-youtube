a9b982e388675d504ee1d8641a329c359fe30c96
bitu1212 <bituk8591@gmail.com>
2026-09-07 05:53:12 +0000

    06_Datesinjs

--- .devcontainer/01_Basics/06_Datesinjs.js
+++ .devcontainer/01_Basics/06_Datesinjs.js
@@ -11,3 +11,11 @@
 let myCreatedDate = new Date(2026 ,0 ,19 ,7 ,5,30 );
 
 console.log(myCreatedDate.toLocaleString());
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.getSeconds);
+
+
+
