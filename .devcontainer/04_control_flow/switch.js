ca750ba1b58cf2de968e2fccd9a2eb9132ec1d7a
bitu1212 <bituk8591@gmail.com>
2026-09-11 07:36:31 +0000

    switch

---
+++ .devcontainer/04_control_flow/switch.js
@@ -0,0 +1,35 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+let month = 3
+
+switch (month ) {
+    case 1:
+        console.log("january");
+        
+        break;
+        case 2:
+        console.log("feb");
+        
+        break;
+        case 3:
+        console.log("march");
+        
+        break;
+        case 4:
+        console.log("aprail");
+        
+        break;
+
+    default:
+        console.log("Default match");
+        
+        break;
+        
+}
+
