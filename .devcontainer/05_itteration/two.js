7217e17e1a7bd6f0406e9b6cca07ac31947c5f3b
bitu1212 <bituk8591@gmail.com>
2026-09-11 10:22:28 +0000

    two

---
+++ .devcontainer/05_itteration/two.js
@@ -0,0 +1,15 @@
+// while
+
+// let index = 0
+// while(index <= 10){
+//     console.log(`Vlue of loop ${index}`);
+//     index = index+1
+    
+// 
+
+let score =11
+
+do{
+    console.log(`value is ${score}`);
+    score++
+}while(score <= 10);
